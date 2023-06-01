1ec947dc4b94da4b3a48c640cceda3802901a3a3
Mariia Kovach <mariiakovach123@gmail.com>
2023-06-01 21:57:11 +0200

    added desktop styles to hero

---
+++ app/(site)/components/Button/index.jsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+const Button = () => (
+  <Link
+    href="https://www.instagram.com/fit77_fitness_studio/"
+    target="_blank"
+    className="button"
+  >
+    Записатися на заняття
+  </Link>
+);
+
+export default Button;
--- app/(site)/sections/hero/index.jsx
+++ app/(site)/sections/hero/index.jsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import { PortableText } from '@portabletext/react';
 import { getHero } from '@/sanity/sanity-utils';
 import { playfair_display } from '../../../fonts';
+import Button from '../../components/Button';
 
 const Hero = async () => {
   const hero = await getHero();
@@ -11,26 +12,31 @@ const Hero = async () => {
       <div className="container">
         <div className="hero">
           <div className="hero__main">
-            <div className="hero__main-left flex-40">
-              <h1 className={`${playfair_display.variable} title hero__main-title`}>
-                <span>Твоя</span>
-                <span>Фітнес</span>
-                <span>Студія</span>
-              </h1>
+            <div className="hero__main-left">
+              <div className={`${playfair_display.variable} title hero__main-title`}>
+                <h1>Твоя</h1>
+                <h1>Фітнес</h1>
+                <h1>Студія</h1>
+              </div>
 
               <div>
                 <p className="hero__main-description">
                   {hero[0].description}
                 </p>
 
-                <p className="hero__main-address">
-                  {hero[0].address}
-                </p>
+                <div className="hero__main-address">
+                  <Image
+                    width={24}
+                    height={24}
+                    src="images/icons/location.svg"
+                    alt="location"
+                  />
+                  <p>{hero[0].address}</p>
+                </div>
               </div>
-
             </div>
 
-            <div className="flex-60">
+            <div className="hero__main-right">
               <div className="hero__main-image-container">
                 <Image
                   src={hero[0].heroImage}
@@ -44,7 +50,7 @@ const Hero = async () => {
           </div>
 
           <div className="hero__intro">
-            <div className="flex-40">
+            <div className="hero__intro-left">
               <div className="hero__intro-image-container">
                 <Image
                   src={hero[0].introImage}
@@ -56,13 +62,66 @@ const Hero = async () => {
               </div>
             </div>
 
-            <div className="flex-60">
+            <div className="hero__intro-right">
               <div className="hero__intro-text">
-                <h3 className={`${playfair_display.variable} title hero__intro-title`}>
-                  {hero[0].introTitle}
-                </h3>
-                <div className="hero__intro-details">
-                  <PortableText value={hero[0].introDetails} />
+                <div>
+                  <h3 className={`${playfair_display.variable} title hero__intro-title`}>
+                    {hero[0].introTitle}
+                  </h3>
+                  <div className="hero__intro-details">
+                    <PortableText value={hero[0].introDetails} />
+                  </div>
+
+                  <div className="hero__services">
+                    <p>У FIT77 ми:</p>
+                    <div>
+                      <div className="hero__services-item">
+                        <Image
+                          width={24}
+                          height={24}
+                          src="images/icons/bicycle.svg"
+                          alt="bicycle"
+                        />
+                        <p>пропонуємо тренування різних напрямків;</p>
+                      </div>
+                      <div className="hero__services-item">
+                        <Image
+                          width={24}
+                          height={24}
+                          src="images/icons/paper.svg"
+                          alt="paper"
+                        />
+                        <p>розробляємо індивідуальні тренувальні програми;</p>
+                      </div>
+                      <div className="hero__services-item">
+                        <Image
+                          width={24}
+                          height={24}
+                          src="images/icons/dumbbell.svg"
+                          alt="dumbbell"
+                        />
+                        <p>забезпечуємо професійним обладнанням;</p>
+                      </div>
+                      <div className="hero__services-item">
+                        <Image
+                          width={24}
+                          height={24}
+                          src="images/icons/heart.svg"
+                          alt="heart"
+                        />
+                        <p>створюємо комфортну атмосферу.</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <p>
+                    Якщо ви хочете займатися спортом та покращити своє здоров&apos;я,
+                    приходьте до нас в FIT77! Ми будемо раді бачити вас у нашій студії.
+                  </p>
+                </div>
+
+                <div className="hero__cta-button">
+                  <Button />
                 </div>
               </div>
             </div>
