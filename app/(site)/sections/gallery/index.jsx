a26584f5fb0236ab5565b76d688cbd263887f81f
Mariia Kovach <mariiakovach123@gmail.com>
2023-07-09 15:45:32 +0200

    added styles to gallery section

--- app/(site)/sections/gallery/index.jsx
+++ app/(site)/sections/gallery/index.jsx
@@ -1,59 +1,42 @@
 'use client';
 
 import Image from 'next/image';
-// import { useState, useRef } from 'react';
+import { useState, useRef } from 'react';
 
-const Gallery = async () => {
-  // const [isDown, setIsDown] = useState(false);
-  // const [startX, setStartX] = useState(null);
-  // const [scrollLeft, setScrollLeft] = useState(0);
-  // const galleryRef = useRef(null);
+const Gallery = () => {
+  const [isDown, setIsDown] = useState(false);
+  const [startX, setStartX] = useState(null);
+  const [scrollLeft, setScrollLeft] = useState(0);
+  const galleryRef = useRef(null);
 
-  // const handleMouseDown = (e) => {
-  //   setIsDown(true);
-  //   setStartX(e.pageX - galleryRef.current.offsetLeft);
-  //   setScrollLeft(galleryRef.current.scrollLeft);
-  // };
+  const handleMouseDown = (e) => {
+    setIsDown(true);
+    setStartX(e.pageX - galleryRef.current.offsetLeft);
+    setScrollLeft(galleryRef.current.scrollLeft);
+  };
 
-  // const handleMouseMove = (e) => {
-  //   if (!isDown) return;
-  //   e.preventDefault();
-  //   const x = e.pageX - galleryRef.current.offsetLeft;
-  //   const walk = (x - startX) * 2;
-  //   galleryRef.current.scrollLeft = scrollLeft - walk;
-  // };
+  const handleMouseMove = (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - galleryRef.current.offsetLeft;
+    const walk = (x - startX) * 2;
+    galleryRef.current.scrollLeft = scrollLeft - walk;
+  };
 
-  // const handleMouseUp = () => {
-  //   setIsDown(false);
-  // };
-
-  let x = 0;
-  // const [startX, setStartX] = useState(0);
-  // const [scrollLeft, setScrollLeft] = useState(0);
-
-  // const handleMouseDown = (e) => {
-  //   setStartX(e.pageX - e.currentTarget.offsetLeft);
-  //   setScrollLeft(e.currentTarget.scrollLeft);
-  // };
-
-  // const handleMouseMove = (e) => {
-  //   e.preventDefault();
-  //   const x = e.pageX - e.currentTarget.offsetLeft;
-  //   const walk = (x - startX);
-  //   e.currentTarget.scrollLeft = scrollLeft - walk;
-  // };
+  const handleMouseUp = () => {
+    setIsDown(false);
+  };
 
   return (
-    <section
-      id="gallery"
-      className="gallery"
-      // ref={galleryRef}
-      // onMouseDown={handleMouseDown}
-      // onMouseMove={handleMouseMove}
-      // onMouseUp={handleMouseUp}
-      // onMouseLeave={handleMouseUp}
-    >
-      <div className="gallery__images">
+    <section id="gallery" className="gallery">
+      <div
+        className="gallery__images"
+        ref={galleryRef}
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
+        onMouseLeave={handleMouseUp}
+      >
         {[...Array(11)].map((_, index) => (
           <div className="gallery__image-container" key={`${index + 1}`}>
             <Image
